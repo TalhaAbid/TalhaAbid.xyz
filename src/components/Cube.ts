1bf5c36d3ca4483265894e38a6d9e675e5ecb678
Talha Abid <talhaabid100@gmail.com>
2021-07-26 13:38:22 -0400

    test

--- src/components/Cube.ts
+++ src/components/Cube.ts
@@ -1,33 +1,32 @@
-import { BoxBufferGeometry, MeshStandardMaterial, Mesh, MathUtils } from 'three'
-
+import { BoxBufferGeometry, MeshStandardMaterial, Mesh } from "three";
 
 const createMaterial = (): MeshStandardMaterial => {
-	// you can add texture loader here for example
-	// const textureLoader = new TextureLoader();
+  // you can add texture loader here for example
+  // const textureLoader = new TextureLoader();
 
-	// create a mesh standard material
-	const material = new MeshStandardMaterial({
-		color: "blue"
-	})
-	return material;
-}
+  // create a mesh standard material
+  const material = new MeshStandardMaterial({
+    color: "blue",
+  });
+  return material;
+};
 
 const createCube = (): Mesh => {
-	// create a geometry
-	const geometry = new BoxBufferGeometry(2, 2, 2);
-	// create the material
-	const material = createMaterial();
+  // create a geometry
+  const geometry = new BoxBufferGeometry(2, 2, 2);
+  // create the material
+  const material = createMaterial();
 
-	// create the mesh containing geometry and material
-	const cube = new Mesh(geometry, material)
-	const radiansPerNSeconds = ((360 / 12) * Math.PI) / 180;
-	(cube as any).tick = (delta: number) => {
-		cube.rotation.z += radiansPerNSeconds * delta;
-		cube.rotation.x += radiansPerNSeconds * delta;
-		cube.rotation.y += radiansPerNSeconds * delta;
-	}
+  // create the mesh containing geometry and material
+  const cube = new Mesh(geometry, material);
+  const radiansPerNSeconds = ((360 / 12) * Math.PI) / 180;
+  (cube as any).tick = (delta: number) => {
+    cube.rotation.z += radiansPerNSeconds * delta;
+    cube.rotation.x += radiansPerNSeconds * delta;
+    cube.rotation.y += radiansPerNSeconds * delta;
+  };
 
-	return cube;
-}
+  return cube;
+};
 
 export { createCube };
